3bf93ad52f2b8dc34a1601b29ed85c5b887bc297
JCamiloo <jcamilo.osorio15@gmail.com>
2020-06-12 23:45:07 -0500

    page view and tabs using provider

--- lib/src/pages/tabs_page.dart
+++ lib/src/pages/tabs_page.dart
@@ -1,22 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class TabsPage extends StatefulWidget {
-  @override
-  _TabsPageState createState() => _TabsPageState();
-}
-
-class _TabsPageState extends State<TabsPage> {
-  
-  int currentIndex = 0;
+class TabsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => _NavigationModel(),
+      child: Scaffold(
+        body: _Pages(),
+        bottomNavigationBar: _Navigation(),
+      ),
+    );
+  }
+}
+
+class _Pages extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    final navigationModel = Provider.of<_NavigationModel>(context);
+
     return Scaffold(
       body: PageView(
-        onPageChanged: (index){
-          setState(() => currentIndex = index);
-        },
+        controller: navigationModel.pageController,
         physics: BouncingScrollPhysics(),
+        onPageChanged: (index) => navigationModel.currentPage = index,
         children: <Widget>[
           Container(
             color: Colors.red,
@@ -26,13 +36,41 @@ class _TabsPageState extends State<TabsPage> {
           ),
         ],
       ),
-      bottomNavigationBar:BottomNavigationBar(
-        currentIndex: currentIndex,
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.person_outline), title: Text('Para ti')),
-          BottomNavigationBarItem(icon: Icon(Icons.public), title: Text('Encabezados'))
-        ],
-      )
     );
   }
+}
+
+class _Navigation extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    final navigationModel = Provider.of<_NavigationModel>(context);
+
+    return BottomNavigationBar(
+      currentIndex: navigationModel.currentPage,
+      onTap: (index) => navigationModel.currentPage = index,
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.person_outline), title: Text('Para ti')),
+        BottomNavigationBarItem(icon: Icon(Icons.public), title: Text('Encabezados'))
+      ],
+    );
+  }
+}
+
+class _NavigationModel with ChangeNotifier {
+
+  int _currentPage = 0;
+  PageController _pageController = PageController();
+
+  int get currentPage => this._currentPage;
+
+  set currentPage(int value) {
+    this._currentPage = value;
+    _pageController.animateToPage(value, duration: Duration(milliseconds: 250), curve: Curves.easeOut);
+    notifyListeners();
+  }
+
+  PageController get pageController => this._pageController;
+
 }

79037b2c87a013745896764d154a77bc96f3df8f
JCamiloo <jcamilo.osorio15@gmail.com>
2020-06-12 23:02:58 -0500

    page view and tabs

--- lib/src/pages/tabs_page.dart
+++ lib/src/pages/tabs_page.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 
-class TabsPage extends StatelessWidget {
+class TabsPage extends StatefulWidget {
+  @override
+  _TabsPageState createState() => _TabsPageState();
+}
+
+class _TabsPageState extends State<TabsPage> {
+  
+  int currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
+        onPageChanged: (index){
+          setState(() => currentIndex = index);
+        },
         physics: BouncingScrollPhysics(),
         children: <Widget>[
           Container(
@@ -15,6 +26,13 @@ class TabsPage extends StatelessWidget {
           ),
         ],
       ),
+      bottomNavigationBar:BottomNavigationBar(
+        currentIndex: currentIndex,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.person_outline), title: Text('Para ti')),
+          BottomNavigationBarItem(icon: Icon(Icons.public), title: Text('Encabezados'))
+        ],
+      )
     );
   }
 }
